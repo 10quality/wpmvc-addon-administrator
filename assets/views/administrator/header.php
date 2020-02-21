6599b2b6c4db938741110b9c2024c3e5d8535243
Alejandro Mostajo <amostajo@gmail.com>
2020-02-21 05:02:42 -0600

    Global descriptions.

--- assets/views/administrator/header.php
+++ assets/views/administrator/header.php
@@ -12,4 +12,7 @@
 <div id="<?php echo esc_attr( $model->id ) ?>" class="wrap wpmvc administrator">
     <?php do_action( 'administrator_header_' . $model->id, $model, $tab ) ?>
     <h2><?php echo $model->title ?></h2>
+    <?php if ( $model->description ) : ?>
+        <p class="description"><?php echo $model->description ?></p>
+    <?php endif ?>
     <?php do_action( 'administrator_after_title_' . $model->id, $model, $tab ) ?>
