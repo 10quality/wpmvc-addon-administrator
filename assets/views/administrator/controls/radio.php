99f096193da97829193c0f5d219f4766cfdb348b
Alejandro Mostajo <amostajo@gmail.com>
2020-02-21 07:19:24 -0600

    Update radio.php
    Fixes radio template.

--- assets/views/administrator/controls/radio.php
+++ assets/views/administrator/controls/radio.php
@@ -11,13 +11,15 @@
 ?>
 <div class="radio-group direction-<?php echo isset( $control ) && array_key_exists( 'direction', $control ) ? esc_attr( $control['direction'] ) : 'row' ?>">
     <?php foreach ( $options as $val => $label ) : ?>
-        <input id="<?php echo esc_attr( $id ) ?>-<?php echo esc_attr( $val ) ?>"
-            type="radio"
-            name="<?php echo esc_attr( isset( $name ) ? $name : $id ) ?>"
-            class="<?php echo esc_attr( implode( ' ', $class ) ) ?>"
-            value="<?php echo esc_attr( $val ) ?>"
-            <?php if ( $value == $val ) : ?>checked="checked"<?php endif ?>
-            <?php echo $html_attributes ?>
-        /> <?php echo $label ?>
+        <label for="<?php echo esc_attr( $id ) ?>-<?php echo esc_attr( $val ) ?>">
+            <input id="<?php echo esc_attr( $id ) ?>-<?php echo esc_attr( $val ) ?>"
+                type="radio"
+                name="<?php echo esc_attr( isset( $name ) ? $name : $id ) ?>"
+                class="<?php echo isset( $class ) ? esc_attr( implode( ' ', $class ) ) : '' ?>"
+                value="<?php echo esc_attr( $val ) ?>"
+                <?php if ( $value == $val ) : ?>checked="checked"<?php endif ?>
+                <?php echo $html_attributes ?>
+            /> <?php echo $label ?>
+        </label>
     <?php endforeach ?>
 </div>
