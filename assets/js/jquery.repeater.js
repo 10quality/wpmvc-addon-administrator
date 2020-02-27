026484b3975e5b41d99632124becbfa9dcc7ce12
Alejandro Mostajo <amostajo@gmail.com>
2020-02-27 03:15:29 -0600

    #6 Fixes evens/odds when deleting.

--- assets/js/jquery.repeater.js
+++ assets/js/jquery.repeater.js
@@ -28,6 +28,7 @@
                 if ( self.$button.length && self.$template.length && self.$items.length ) {
                     self.$button.on( 'click', self.methods.on_add );
                     self.key = self.$items.find( '*[data-repeater-field="1"]' ).length;
+                    self.methods.init_actions();
                 }
                 self.$el.on( 'click', '*[role="repeater-remove"]', self.methods.on_remove )
             },
@@ -65,9 +66,42 @@
                 if ( event !== undefined )
                     event.preventDefault();
                 var key = $( this ).closest( '*[data-repeater-field="1"]' ).data( 'repeater-key' );
-                if ( key && confirm( self.$el.data( 'remove-message' ) ) )
+                if ( key !== undefined && confirm( self.$el.data( 'remove-message' ) ) ) {
                     self.$items.find( '*[data-repeater-key="' + key + '"]' ).remove();
+                    self.methods.update_evens_odds();
+                }
             },
+            init_actions: function() {
+                var keys = [];
+                self.$items.find( '*[data-repeater-field="1"]' ).each( function() {
+                    var key = $( this ).data( 'repeater-key' );
+                    if ( key !== undefined
+                        && keys.find( function( existing ) { return existing === key; } ) === undefined
+                    ) {
+                        keys.push( key );
+                        $( this ).find( '*[role="repeater-actions"]' ).html(
+                            $( document ).find( '#repeater-actions' ).html()
+                        );
+                    }
+                } );
+            },
+            update_evens_odds: function()
+            {
+                var keys = [];
+                self.$items.find( '*[data-repeater-field="1"]' ).each( function() {
+                    var key = $( this ).data( 'repeater-key' );
+                    if ( key !== undefined
+                        && keys.find( function( existing ) { return existing === key; } ) === undefined
+                    ) {
+                        keys.push( key );
+                    }
+                } );
+                for ( var i = 0; i < keys.length; i++ ) {
+                    var is_odd = ( i + 1 ) % 2;
+                    self.$items.find( '*[data-repeater-key="' + keys[i] + '"]' ).removeClass( is_odd ? 'repeater-even' : 'repeater-odd' );
+                    self.$items.find( '*[data-repeater-key="' + keys[i] + '"]' ).addClass( is_odd ? 'repeater-odd' : 'repeater-even' );
+                }
+            }
         };
         self.methods.ready();
     };
