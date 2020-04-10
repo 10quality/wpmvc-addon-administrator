40e7c3477d8f3f7a86b9154bedbdaf493a5f13ae
Alejandro Mostajo <amostajo@gmail.com>
2020-04-10 00:29:48 -0600

    #16 Fixes data attributes for datepicker.

--- assets/js/jquery.datepicker.js
+++ assets/js/jquery.datepicker.js
@@ -3,7 +3,7 @@
  * @author 10 Quality <info@10quality.com>
  * @package wpmvc-addon-administrator
  * @license MIT
- * @version 1.0.4
+ * @version 1.0.5
  */
 ( function( $ ) { $( document ).ready( function() {
     /**
@@ -12,11 +12,11 @@
      */
     $( '.datepicker' ).each( function() {
         $( this ).datepicker( {
-            showOtherMonths: $( this ).data( 'show-other-months' ) || true,
-            selectOtherMonths: $( this ).data( 'select-other-months' ) || true,
-            showButtonPanel: $( this ).data( 'show-button-panel' ) || false,
-            changeMonth: $( this ).data( 'change-month' ) || false,
-            changeYear: $( this ).data( 'change-year' ) || false,
+            showOtherMonths: $( this ).data( 'show-other-months' ) !== undefined ? $( this ).data( 'show-other-months' ) : true,
+            selectOtherMonths: $( this ).data( 'select-other-months' ) !== undefined ? $( this ).data( 'select-other-months' ) : true,
+            showButtonPanel: $( this ).data( 'show-button-panel' ) !== undefined ? $( this ).data( 'show-button-panel' ) : false,
+            changeMonth: $( this ).data( 'change-month' ) !== undefined ? $( this ).data( 'change-month' ) : false,
+            changeYear: $( this ).data( 'change-year' ) !== undefined ? $( this ).data( 'change-year' ) : false,
             dateFormat: $( this ).data( 'date-format' ) || 'yy-mm-dd',
         } );
     } );
@@ -32,11 +32,11 @@
         if ( $items.find( '*[data-repeater-key="' + key + '"] .datepicker' ).length ) {
             var $datepicker = $items.find( '*[data-repeater-key="' + key + '"] .datepicker' );
             $datepicker.datepicker( {
-                showOtherMonths: $datepicker.data( 'show-other-months' ) || true,
-                selectOtherMonths: $datepicker.data( 'select-other-months' ) || true,
-                showButtonPanel: $datepicker.data( 'show-button-panel' ) || false,
-                changeMonth: $datepicker.data( 'change-month' ) || false,
-                changeYear: $datepicker.data( 'change-year' ) || false,
+                showOtherMonths: $datepicker.data( 'show-other-months' ) !== undefined ? $datepicker.data( 'show-other-months' ) : true,
+                selectOtherMonths: $datepicker.data( 'select-other-months' ) !== undefined ? $datepicker.data( 'select-other-months' ) : true,
+                showButtonPanel: $datepicker.data( 'show-button-panel' ) !== undefined ? $datepicker.data( 'show-button-panel' ) : false,
+                changeMonth: $datepicker.data( 'change-month' ) !== undefined ? $datepicker.data( 'change-month' ) : false,
+                changeYear: $datepicker.data( 'change-year' ) !== undefined ? $datepicker.data( 'change-year' ) : false,
                 dateFormat: $datepicker.data( 'date-format' ) || 'yy-mm-dd',
             } );
         }
