e768124d9f672d90688e14a32f7f27e3ed980317
Alejandro Mostajo <amostajo@gmail.com>
2020-04-10 00:39:36 -0600

    #16 Autocomple control option added for input based controls.

--- assets/views/administrator/controls/datepicker.php
+++ assets/views/administrator/controls/datepicker.php
@@ -15,5 +15,10 @@ $class = array_merge( ['datepicker'], isset( $class ) ? $class : [] );
     name="<?php echo esc_attr( isset( $name ) ? $name : $id ) ?>"
     class="<?php echo esc_attr( implode( ' ', $class ) ) ?>"
     value="<?php echo esc_attr( $value ) ?>"
+    <?php if ( isset( $control ) && isset( $control['autocomplete'] ) && $control['autocomplete'] ) ?>
+        autocomplete="on"
+    <?php else : ?>
+        autocomplete="off"
+    <?php endif ?>
     <?php echo $html_attributes ?>
 />
