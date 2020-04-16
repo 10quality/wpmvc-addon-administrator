20a5a40aa83468fc5b75a5e8d2ec5afa5d162efb
Alejandro Mostajo <amostajo@gmail.com>
2020-04-16 07:04:51 -0600

    #18 Fixes bugs.

--- assets/views/administrator/controls/media.php
+++ assets/views/administrator/controls/media.php
@@ -18,7 +18,7 @@ $class = array_merge( ['media-uploader', 'button'], isset( $class ) ? $class : [
         name="<?php echo esc_attr( isset( $name ) ? $name : $id ) ?>"
         value="<?php echo esc_attr( $value ) ?>"
         data-editor="editor-<?php echo esc_attr( $id ) ?>"
-        <?php if ( !isset( $control ) || !array_key_exists( 'wide', $control ) && $control['wide'] ) : ?>
+        <?php if ( isset( $control ) && array_key_exists( 'wide', $control ) && $control['wide'] ) : ?>
             data-input-css="widefat"
         <?php endif ?>
         <?php echo $html_attributes ?>
@@ -26,7 +26,7 @@ $class = array_merge( ['media-uploader', 'button'], isset( $class ) ? $class : [
         <?php if ( !isset( $control ) || !array_key_exists( 'show_icon', $control ) || $control['show_icon'] === true ) : ?>
             <span class="dashicons dashicons-admin-media"></span>
         <?php endif ?>
-        <?php if ( !isset( $control ) || !array_key_exists( 'icon', $control ) ) : ?>
+        <?php if ( isset( $control ) && array_key_exists( 'icon', $control ) ) : ?>
             <i class="fa <?php echo esc_attr( $control['icon'] ) ?>" aria-hidden="true"></i>
         <?php endif ?>
         <?php echo isset( $control ) && array_key_exists( 'button_label', $control ) ? $control['button_label'] : __( 'Add media' ) ?>
